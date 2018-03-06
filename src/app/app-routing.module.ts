17db5c43068eb6e7607b26cc76bc1564701f8685
Garmhausen <robetgarmhausen@gmail.com>
2018-03-06 12:53:48 -0500

    Add routes for each component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PlayComponent } from './play/play.component';
+import { WinComponent } from './win/win.component';
+import { LoseComponent } from './lose/lose.component';
+import { WinnersComponent } from './winners/winners.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'play',
+    pathMatch: 'full',
+    component: PlayComponent
+  },
+  {
+    path: 'win',
+    pathMatch: 'full',
+    component: WinComponent
+  },
+  {
+    path: 'lose',
+    pathMatch: 'full',
+    component: LoseComponent
+  },
+  {
+    path: 'winners',
+    pathMatch: 'full',
+    component: WinnersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
