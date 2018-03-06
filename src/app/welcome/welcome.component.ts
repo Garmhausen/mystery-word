1b826e608382974376baa87940474837c94fcd2a
Garmhausen <robetgarmhausen@gmail.com>
2018-03-06 15:15:15 -0500

    Complete functional Welcome menu.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Store } from '@ngrx/store';
 
 import { GameService } from '../game.service';
+
+import { State } from '../shared/reducers';
+import * as GameActions from '../shared/actions/game';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  storeSub: any;
+  game: any;
+
+  constructor(private store: Store<State>, private gameService: GameService) { }
 
   ngOnInit() {
+    this.storeSub = this.store.select('game').subscribe(game => {
+      this.game = game;
+    });
+  }
+
+  ngOnDestroy() {
+    this.storeSub.unsubscribe();
+  }
+
+  startGame(difficulty) {
+    this.gameService.getNewGame(difficulty).then((r: any) => {
+        console.log('game =', r);
+        this.store.dispatch(new GameActions.UpdateGame(r));
+      }
+    );
   }
 
 }
