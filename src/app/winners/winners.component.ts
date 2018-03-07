06d4435f78d3e2cf3c96d15b556e86382e5dd474
Garmhausen <robetgarmhausen@gmail.com>
2018-03-07 12:21:10 -0500

    winners comp logic and template, with styling.

--- src/app/winners/winners.component.ts
+++ src/app/winners/winners.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GameService } from '../game.service';
+import { Utils } from '../shared/utils';
+
 @Component({
   selector: 'app-winners',
   templateUrl: './winners.component.html',
   styleUrls: ['./winners.component.scss']
 })
 export class WinnersComponent implements OnInit {
+  winners: any[] = [];
 
-  constructor() { }
+  constructor(public utils: Utils, private gameService: GameService) { }
 
   ngOnInit() {
+    this.gameService.getWinners().then((r: any) => {
+      this.winners = r;
+    });
   }
-
 }
